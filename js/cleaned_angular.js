efd50ff650dbcd6b1c05d7ea3972e29466204d42
audreyseo <audreyleeseo@gmail.com>
2016-07-05 14:18:26 -0400

    Created the cookieSavedData module
    This will hopefully replace the very bulky code that I currently have in the ContentController controller module. cookieSavedData is a factory service that is able to get, set, and remove data from the factory, which holds all of the data for flash cards, and I really should rename this to the flashCardData factory module.
    Other methods included this factory are wrappers for saving all of the flashcard data in cookies.

--- js/cleaned_angular.js
+++ js/cleaned_angular.js
@@ -73,12 +73,26 @@ app.value('cases', [
 app.value('cookieStrings', {
 	hiddenRows: "OLLHiddenRowsCookie", 
 	hiddenCols: "OLLHiddenCookie", 
-	maxFlashCards: "OLLMaxNumCards",
+	cards: "OLLMaxNumCards",
 	practicing: "OLLPracticing",
 	practiceCards: "OLLPracticeCards",
 	cardOptions: "OLLCardOptions",
-	cardPriorities: "OLLCardPriorities"});
-
+	cardPriorities: "OLLCardPriorities",
+	dataCards: "OLLDataCards"});
+app.value('cookieData', {
+			practicing: {},
+			practiceCards: [],
+			cardPriorities: {},
+			cardOptions: {},
+			practiceButton: "Select cases to practice"
+	});
+app.value('cookieDataTypes', {
+			practicing: "object",
+			practiceCards: "object",
+			cardPriorities: "object",
+			cardOptions: "object",
+			practiceButton: "string"
+});
 app.value('hiddenRowsY', {});
 
 
@@ -142,7 +156,144 @@ app.directive('ollRow', function() {
 });
 
 
-app.controller('ContentController', ['$scope', 'cases', 'hiddenRowsY', 'cookieStrings','$cookies',  'orderByFilter', 'filterFilter', function($scope, cases, hiddenRows, cookieString, $cookies, orderBy, filterFilter) {
+app.factory('cookieSavedData', ['cookieStrings', '$cookies', 'cookieData', function(cookieStringData, $cookies, cookieData) {
+	var factory = {data: cookieData, cookies: cookieStringData, cards: {}};
+	
+	factory.initialize = function() {
+		for (var ind in factory.data) {
+			if (!angular.isString(factory.data[ind])) {
+				try {
+				 factory.data[ind] = $cookies.getObject(factory.cookies[ind]);
+				} catch(e) {
+					factory.data[ind] = cookieData[ind];
+					$cookies.putObject(factory.cookies[ind], factory.data[ind]);
+				}
+				console.log(ind + ": " + angular.toJson(factory.data[ind]));
+			}
+		}
+		
+		for (var i in factory.data.practiceCards) {
+			factory.cards[factory.data.practiceCards[i].code] = factory.data.practiceCards[i];
+		}
+		
+		if (!angular.isNumber(factory.data.cards.maxNumber)) {
+//		$scope.cards = {maxNumber: 0, options: []};
+			factory.data.cards.maxNumber = parseInt(factory.data.cards.maxNumber);
+			$cookies.putObject(factory.cookies.cards, factory.data.cards);
+		}
+	};
+	
+	factory.isPracticing = function(value) {
+		if (factory.practicing[value] !== undefined) {
+			return practicing[value];
+		}
+		return false;
+	};
+	
+	factory.setPracticing = function(caseCode, value) {
+		factory.data.practicing[caseCode] = value;
+	};
+	
+	factory.removePracticing = function(caseCode ) {
+		factory.setPracticing(caseCode, undefined);
+	};
+	
+	factory.savePracticing = function(newValue) {
+		$cookies.putObject(factory.cookies.practicing, newValue);
+	};
+	
+	factory.getPriority = function(caseCode) {
+		if (angular.isDefined(factory.data.cardPriorities[caseCode])) {
+			return factory.data.cardPriorities[caseCode];
+		} else {
+			return(-1);
+		}
+	};
+	
+	factory.setPriority = function(caseCode, value) {
+		factory.data.cardPriorities[caseCode] = value;
+	};
+	
+	factory.removePriority = function(caseCode) {
+		factory.setPriority(caseCode, undefined);
+	};
+	
+	factory.saveCardPriorities = function(newValue) {
+		$cookies.putObject(factory.cookies.cardPriorities, newValue);
+	};
+	
+	factory.getCard = function(caseCode) {
+		if (angular.isDefined(factory.cards[caseCode])) {
+			return(factory.cards[caseCode]);
+		}
+		return -1;
+	};
+	
+	factory.setCard = function(caseCode, cardValue) {
+		if (isUndefined(factory.cards[caseCode])) {
+			factory.cards[caseCode] = cardValue;
+			factory.data.practiceCards.push(cardValue);
+		} else {
+			factory.cards[caseCode] = cardValue;
+			for (var ind in factory.data.practiceCards) {
+				if (factory.data.practiceCards[ind].code === caseCode) {
+					factory.data.practiceCards.splice(ind, 1);
+					factory.data.practiceCards.push(caseCode, cardValue);
+					break;
+				}
+			}
+		}
+	};
+	
+	factory.removeCard = function(caseCode) {
+		if (isUndefined(factory.cards[caseCode])) {
+			return;
+		} else {
+			factory.cards[caseCode] = undefined;
+			for (var ind in factory.data.practiceCards) {
+				if (factory.data.practiceCards[ind.code] == caseCode) {
+					factory.data.practiceCards.splice(ind, 1);
+					break;
+				}
+			}
+		}
+	};
+	
+	factory.savePracticeCards = function(newValue) {
+		$cookies.putObject(factory.cookies.practiceCards, newValue)
+	}
+	
+	factory.saveCards = function(newValue) {
+		$cookies.putObject(factory.cookies.dataCards, newValue);
+	}
+	
+	factory.getAlgorithmOption = function(caseCode) {
+		if (angular.isDefined(factory.data.cardOptions[caseCode])) {
+			return factory.data.cardOptions[caseCode];
+		}
+		return -1;
+	};
+	
+	factory.setAlgorithmOption = function(caseCode, value) {
+		factory.data.cardOptions[caseCode] = value;
+	}
+	
+	factory.removeAlgorithmOption = function(caseCode) {
+		factory.setAlgorithmOption(caseCode, undefined);
+	}
+	
+	factory.saveAlgorithmOptions = function(newValue) {
+		$cookies.putObject(factory.cookies.cardOptions, newValue);
+	}
+	
+	
+	return(factory);
+}]);
+
+
+app.controller('ContentController', ['$scope', 'cases', 'hiddenRowsY', 'cookieStrings', 'cookieSavedData', '$cookies',  'orderByFilter', 'filterFilter', function($scope, cases, hiddenRows, cookieString, cookieData, $cookies, orderBy, filterFilter) {
+		cookieData.initialize();	
+	
 		$scope.cases = cases;
 		$scope.cookieString = cookieString;
 		$scope.hiddenRows = hiddenRows;
@@ -167,14 +318,17 @@ app.controller('ContentController', ['$scope', 'cases', 'hiddenRowsY', 'cookieSt
 			// Have cards, practicing, cardOptions, cardPriorities, and practiceCards
 			$scope.cards = {maxNumber: 0, options: []};
 			try {
-				$scope.cards = $cookies.getObject($scope.cookieString.maxFlashCards);
-				console.log(typeof $scope.cards.maxNumber);
-				console.log(typeof $scope.cards.options);
-				console.log(typeof $scope.cards);
+				$scope.cards = $cookies.getObject($scope.cookieString.cards);
+//				console.log((typeof $scope.cards.maxNumber));
+//				console.log((typeof $scope.cards.options));
+				console.log("Object == typeof $scope.cards: " + ("object" === (typeof $scope.cards)));
+				console.log("Array == [1,2,3]: " + ("object" === (typeof [1,2,3])));
+//				console.log("Why???: " + (typeof $scope.cards));
+				
 				if (!angular.isNumber($scope.cards.maxNumber)) {
 //					$scope.cards = {maxNumber: 0, options: []};
 					$scope.cards.maxNumber = parseInt($scope.cards.maxNumber);
-					$cookies.putObject($scope.cookieString.maxFlashCards, $scope.cards);
+					$cookies.putObject($scope.cookieString.cards, $scope.cards);
 				}
 			} catch(e) {
 				$scope.cards = {maxNumber: 0, options: []};
@@ -312,10 +466,10 @@ app.controller('ContentController', ['$scope', 'cases', 'hiddenRowsY', 'cookieSt
 		
 		$scope.$watchCollection('cards', function(newValue, oldValue) {
 			// Content, flashcards
-			console.log("Watched cards: " + angular.toJson(newValue));
-			$cookies.putObject($scope.cookieString.maxFlashCards, newValue);
-			console.log("New Value: " + newValue.maxNumber);
-			console.log("New cookie: " + angular.toJson($cookies.getObject($scope.cookieString.maxFlashCards)));
+//			console.log("Watched cards: " + angular.toJson(newValue));
+			$cookies.putObject($scope.cookieString.cards, newValue);
+//			console.log("New Value: " + newValue.maxNumber);
+//			console.log("New cookie: " + angular.toJson($cookies.getObject($scope.cookieString.cards)));
 			if (newValue.maxNumber !== oldValue.maxNumber) {
 				$scope.addPriorityOptions();
 			}
@@ -768,7 +922,7 @@ app.controller('timerController', ['$scope', '$interval', '$cookies', function($
     	}
     };
     
-    console.log("help");
+//    console.log("help");
     
     $scope.reset();
 	}]);
