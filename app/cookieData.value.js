2b92aaa6e09bc4461195020989fdbaa18d920f4a
audreyseo <audreyleeseo@gmail.com>
2016-07-07 16:12:49 -0400

    Added credit for a function found on StackOverFlow

--- app/cookieData.value.js
+++ app/cookieData.value.js
@@ -18,6 +18,8 @@ angular
 	});
 
 
+// Found on StackOverFlow:
+// http://stackoverflow.com/questions/3895478/does-javascript-have-a-method-like-range-to-generate-an-array-based-on-suppl
 function range(start, count) {
   return Array.apply(0, Array(count))
     .map(function (element, index) { 
