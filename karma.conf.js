9128fb4ab8ae7a9ce9d29e3339a821d510a398aa
audreyseo <audreyleeseo@gmail.com>
2016-07-14 19:57:51 -0400

    Modified karma.conf.js
    Needed to modify karma.conf.js because I forgot to add '.xml' to the end
    of junit reports.

--- karma.conf.js
+++ karma.conf.js
@@ -67,7 +67,7 @@ module.exports = function(config) {
     
     junitReporter: {
       outputDir: 'junit', // results will be saved as $outputDir/$browserName.xml
-      outputFile: (new Date()).toTimeString(), // if included, results will be saved as $outputDir/$browserName/$outputFile
+      outputFile: (new Date()).toTimeString() + ".xml", // if included, results will be saved as $outputDir/$browserName/$outputFile
       suite: '', // suite will become the package name attribute in xml testsuite element
       useBrowserName: true, // add browser name to report and classes names
       nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
