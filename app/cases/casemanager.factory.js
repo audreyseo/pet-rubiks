3c6e26027f95d082d46e7e5771d46a6cb5513daa
audreyseo <audreyleeseo@gmail.com>
2016-08-20 22:24:44 -0400

    Moved oll images, added pll images, tweakes pll/ollCases
    I finally added the image urls to the pll and oll cases values, using
    regexes!!! Very exciting things, very exciting things.
    
    I also added folders for the the different cases, pll and oll, that
    contain all of the images. So now I need to remove the code that says
    that we need to add the img property...I also need to double check to
    make sure that it is the 'img' property that we're looking for...

--- app/cases/casemanager.factory.js
+++ app/cases/casemanager.factory.js
@@ -9,7 +9,7 @@ CaseManager.$inject = ['ollCases', 'pllCases'];
 
 function CaseManager(ollCases, pllCases) {
 	var factory = {oll: ollCases, pll: pllCases};
-	
+
 	factory.fetchImage = fetchImage;
 	factory.isOLL = isOLL;
 	factory.isOLLCase = isOLLCase;
@@ -18,38 +18,38 @@ function CaseManager(ollCases, pllCases) {
 	factory.setStage = setStage;
 	factory.stage = "";
 	factory.type = type;
-	
+
 	factory.makeImages();
 	factory.mapCaseToNumber();
-	
+
 	function fetchImage(code) {
 		if (angular.isNumber(code)) {
-			if (factory.type() === "PLL") {
+			if (factory.isPLL()) {
 				return(factory.pll.img[i]);
-			} else if (factory.type() === "OLL"){
+			} else if (factory.isOLL()){
 				return(factory.oll.img[i]);
 			}
 		} else {
 			if (factory.isOLLCase(code)) {
-				
+
 			} else {
-				
+
 			}
 		}
 	}
-	
+
 	function isOLL() {
 		return factory.stage === "OLL";
 	}
-	
+
 	function isOLLCase(string) {
 		return string.search(/\d/) > -1;
 	}
-	
+
 	function isPLL() {
 		return factory.stage === "PLL";
 	}
-	
+
 	function makeImages() {
 		for (var i = 0; i < factory.oll.length; i++) {
 			factory.oll[i].img = "/img/" + factory.oll[i].code + ".png";
@@ -58,23 +58,23 @@ function CaseManager(ollCases, pllCases) {
 			factory.pll[i].img = "/img/" + factory.pll[i].code + ".png";
 		}
 	}
-	
+
 	function mapCaseToNumber() {
 		factory.ollMap = {};
 		for (var i = 0; i < factory.oll.length; i++) {
 			factory.ollMap[factory.oll[i].code] = factory.oll[i].num;
 		}
-		
+
 		factory.pllMap = {};
 		for (i = 0; i < factory.pll.length; i++) {
 			factory.pllMap[factory.pll[i].code] = i;
 		}
 	}
-	
+
 	function setStage(string) {
 		factory.stage = string;
 	}
-	
+
 	function type() {
 		if (factory.stage === "PLL") {
 			return("PLL");
@@ -82,6 +82,6 @@ function CaseManager(ollCases, pllCases) {
 			return("OLL");
 		}
 	}
-	
+
 	return factory;
 }
